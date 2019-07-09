e7a2c0014c6da72fefb8019ce2d1fd532edd59d9
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-07-08 17:13:39 -0700

    More progress.

--- code-challenges/reg-ex-pt1/challenges-04.test.js
+++ code-challenges/reg-ex-pt1/challenges-04.test.js
@@ -27,10 +27,10 @@ Return an array containing all the matches.
 ------------------------------------------------------------------------------------------------ */
 
 const isCapitalized = (str) => {
-  let regEx = /[A-Z]/g;
+  let regEx = /\b[A-Z]\w*/g;
   console.log(regEx);
 
-  return str.match(regEx);
+  return str.match(regEx) || [];
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -86,7 +86,8 @@ For example, 'Welcome to Code 301!' will return 'W_lc_m_ t_ C_d_ 301!'.
 ------------------------------------------------------------------------------------------------ */
 
 let hangman = (str) => {
-  // Solution code here...
+  const match = /[AaEeIiOoUu]/g;  
+  return str.replace(match, '_');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -191,7 +192,7 @@ xdescribe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   let startString = 'This is a regex challenge. We are trying to create a hangman phrase where all of the vowels are missing!';
 
   test('It should remove the vowels from the hangman string and replace them with underscores', () => {
