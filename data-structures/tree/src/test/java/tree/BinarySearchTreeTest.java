f4659b5da668df01020d8bd48617464bc2f0109e
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-08-26 14:34:08 -0700

    Completed tests and updated README.

--- data-structures/tree/src/test/java/tree/BinarySearchTreeTest.java
+++ data-structures/tree/src/test/java/tree/BinarySearchTreeTest.java
@@ -1,18 +1,42 @@
 package tree;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
-public class BinarySearchTreeTest {
+public class BinarySearchTreeTest
+{
+    BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+    @Before
+    public void initializeTestData()
+    {
+        binarySearchTree.add(7);
+        binarySearchTree.add(8);
+        binarySearchTree.add(2);
+        binarySearchTree.add(4);
+        binarySearchTree.add(12);
+    }
 
     @Test
     public void testAdd()
     {
+        String inOrderResult = Arrays.toString(binarySearchTree.inOrder());
+        assertEquals("Add successfully inserts new nodes into the tree.", "[2, 4, 7, 8, 12]", inOrderResult);
     }
 
     @Test
-    public void testContains()
+    public void testContains_True()
     {
+        assertEquals("Contains returns true for the given value.", true, binarySearchTree.contains(2));
+    }
+
+    @Test
+    public void testContains_False()
+    {
+        assertEquals("Contains returns true for the given value.", false, binarySearchTree.contains(78));
     }
 }
